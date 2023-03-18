b84af4b99777cb173be7bf6d8d36aff9f0b06dc7
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 18:02:26 +0800

    feat(app) : shows maps by bottom navigation view

--- app/angkot_ku/lib/maps.dart
+++ app/angkot_ku/lib/maps.dart
@@ -1,3 +1,5 @@
+import 'package:angkot_ku/user/user_layout.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 LatLng decodeLatLng(dynamic loc) {
@@ -5,4 +7,11 @@ LatLng decodeLatLng(dynamic loc) {
   var longitude = loc["longitude"];
 
   return LatLng(latitude, longitude);
+}
+
+Future<LatLng> getCurrentLatLng() async {
+  await checkPermission();
+  var position = await Geolocator.getCurrentPosition();
+
+  return LatLng(position.latitude, position.longitude);
 }
