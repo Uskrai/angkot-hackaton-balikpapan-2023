f860143e8d9b73ef93947b847f22f174019cc2e2
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 19:16:40 +0800

    feat(app) : route chooser container

---
+++ app/angkot_ku/lib/user/user_bis_layout.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UserBisLayout extends StatefulWidget {
+  const UserBisLayout({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _UserBisLayoutState();
+
+}
+
+class _UserBisLayoutState extends State<UserBisLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+
+}
