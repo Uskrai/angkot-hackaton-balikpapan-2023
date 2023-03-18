16ca7bcf9873d0db0ca9d3ad8344ba56df5fb5da
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-03-18 10:58:12 +0800

    feat(server): implement session from request

--- server/src/session.rs
+++ server/src/session.rs
@@ -27,3 +27,49 @@ pub async fn generate_session(
     .await
     .map(|_| rand_string)
 }
+
+#[derive(Clone)]
+pub struct Session {
+    pub bearer: axum_auth::AuthBearer,
+}
+
+impl Session {
+    pub async fn get_user_id(
+        &self,
+        db: &DatabaseConnection,
+    ) -> Result<entity::session::Model, Error> {
+        let it = entity::session::Entity::find_by_id(self.bearer.0.clone())
+            .one(db)
+            .await?;
+
+        let it = match it {
+            Some(it) => it,
+            None => {
+                return Err(Error::Unauthorized(
+                    crate::error::UnauthorizedType::InvalidSessionId,
+                ))
+            }
+        };
+
+        Ok(it)
+    }
+}
+
+#[async_trait]
+impl<S> FromRequest<S> for Session
+where
+    S: Send,
+{
+    type Rejection = Error;
+
+    async fn from_request(
+        req: &mut axum::extract::RequestParts<S>,
+    ) -> Result<Self, Self::Rejection> {
+        let bearer = req
+            .extract::<axum_auth::AuthBearer>()
+            .await
+            .map_err(|(code, err)| Error::CustomStatus(code, anyhow::anyhow!(err)))?;
+
+        Ok(Session { bearer })
+    }
+}
