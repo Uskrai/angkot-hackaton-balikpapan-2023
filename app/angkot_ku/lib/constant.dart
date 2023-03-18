bda9f761bb3a34d8c37dc3a045352ff40a151f78
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 10:25:09 +0800

    flutter : inital commit project

--- app/angkot_ku/lib/constant.dart
+++ app/angkot_ku/lib/constant.dart
@@ -1,8 +1,2 @@
 
-import 'dart:ui';
-
-import 'package:flutter/widgets.dart';
-
-//Color
-const mPrimaryColor = Color(0xFF65BAE2);
-const mSecondaryColor = Color(0x0060C2FF);
+// const mPrimayColor = Color()
