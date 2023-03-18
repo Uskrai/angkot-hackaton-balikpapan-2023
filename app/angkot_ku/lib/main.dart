e302f091e8ded27dca095499da85eca0348eb6e0
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 10:25:09 +0800

    flutter : inital commit project

--- app/angkot_ku/lib/main.dart
+++ app/angkot_ku/lib/main.dart
@@ -1,97 +1,22 @@
-import 'package:angkot_ku/authentication/login_layout.dart';
-import 'package:angkot_ku/user/user_layout.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_map/plugin_api.dart';
-
-import 'authentication/register_layout.dart';
-import 'client/ApiClient.dart';
-import 'delete/dummy_route.dart';
-import 'delete/route.dart';
 
 void main() {
-  var route = createDummyRoute();
-  var apiClient = ApiClient(url: "192.168.2.34:3000");
-  runApp(
-    MyApp(
-      routes: route,
-      apiClient: apiClient,
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({
-    super.key,
-    required this.routes,
-    required this.apiClient,
-  });
-
-  final RoleRoute routes;
-  final ApiClient apiClient;
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    widget.apiClient.authenticationStream.listen((_) {
-      setState(() {});
-    });
-    super.initState();
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AngkotKu',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
-      home: Builder(
-        builder: (context) {
-          switch (widget.apiClient.authenticationStatus) {
-            // case AuthenticationStatus.authenticated:
-            //   switch (widget.apiClient.auth!.roles[0].name) {
-            //     case RoleType.customer:
-            //       return // TODO()
-            //         HomePassengerWidget(
-            //         routes: widget.routes,
-            //         apiClient: widget.apiClient,
-            //       );
-            //     case RoleType.sharedTaxi:
-            //       return HomeSharedTaxi(
-            //           apiClient: widget.apiClient,
-            //           routes: widget.routes.sharedTaxi);
-            //     case RoleType.bus:
-            //       return HomeBus(
-            //         apiClient: widget.apiClient,
-            //         routes: widget.routes.bus,
-            //       );
-            //   }
-            default:
-              // it should be login layout
-              // return LoginLayout(
-              //   apiClient: widget.apiClient,
-              //   onLoggedIn: () {
-              //     setState(() {});
-              //   },
-              // );
-              return HomeUserLayout();
-          }
-        },
-      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -99,14 +24,6 @@ class _MyAppState extends State<MyApp> {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -119,11 +36,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -143,12 +55,34 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: FlutterMap(options: MapOptions(), children: [
-          TileLayer(
-            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-            userAgentPackageName: "org.github.uskrai.angkot",
-          )
-        ]),
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
