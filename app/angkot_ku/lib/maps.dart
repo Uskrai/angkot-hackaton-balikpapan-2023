05c6ad108f6333af0d8662d5834125e6ff672e03
NawfalHadi <nawfalhdi@gmail.com>
2023-03-19 07:42:03 +0800

    fix(app) : maps

--- app/angkot_ku/lib/maps.dart
+++ app/angkot_ku/lib/maps.dart
@@ -90,9 +90,55 @@ Marker? markerFromUser(User user) {
       point: user.location,
       width: 20,
       height: 20,
-      builder: (context) {
-        return Image.asset("assets/icon/shared-taxi.png");
-      },
+      builder: (context) => GestureDetector(
+        onTap: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.directions_bus,
+                      size: 80,
+                      color: Colors.grey[400],
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Ahmad Submul',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'ID: ANGKOT215',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    FloatingActionButton(
+                      onPressed: () {
+
+                      },
+                      child: const Icon(
+                        Icons.waving_hand,
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        child: Image.asset("assets/icon/shared-taxi.png"),
+      ),
     );
   } else if (user is Bus) {
     return Marker(
