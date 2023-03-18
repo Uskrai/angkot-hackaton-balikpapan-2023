d0c98d2c7c481a859aaf7ca7145190c609b01a1d
Muhammad Fathurrohim <m.faturrohim13@gmail.com>
2023-03-18 10:04:41 +0800

    feat(server): create user table migration

---
+++ server/migration/src/lib.rs
@@ -0,0 +1,14 @@
+pub use sea_orm_migration::prelude::*;
+
+mod m20221126_154332_create_user_table;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![
+            Box::new(m20221126_154332_create_user_table::Migration),
+        ]
+    }
+}
