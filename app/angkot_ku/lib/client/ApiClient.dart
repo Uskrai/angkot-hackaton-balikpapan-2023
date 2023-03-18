a8163a13fae9678af8e02b4dba26fa419d63efd1
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 22:10:54 +0800

    feat(app) : setup response routes

--- app/angkot_ku/lib/client/ApiClient.dart
+++ app/angkot_ku/lib/client/ApiClient.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:angkot_ku/client/VehicleType.dart';
+import 'package:angkot_ku/temp/route.dart';
+import 'package:latlong2/latlong.dart';
+
 import 'Authenticated.dart';
 import 'package:http/http.dart' as http;
 
@@ -21,6 +25,45 @@ class ApiClient {
   Stream get authenticationStream => _authenticationController.stream;
 
   var authenticationStatus = AuthenticationStatus.unkown;
+
+  void handleRoutesResponse(http.Response response) {
+    var body = response.body;
+    dynamic json;
+
+    try {
+      json = jsonDecode(response.body);
+    } catch (_) {
+      throw body;
+    }
+
+    var error = json["message"];
+    if (error != null) {
+      throw error;
+    }
+
+    var data = json["data"];
+    var session = data["session"];
+
+    List<Role> roles = [];
+    for (var role in data["roles"]) {
+      RoleType name;
+      switch (role["name"]) {
+        case "Customer":
+          name = RoleType.customer;
+          break;
+        case "Shared Taxi":
+          name = RoleType.sharedTaxi;
+          break;
+        case "Bus":
+          name = RoleType.bus;
+          break;
+        default:
+          name = RoleType.customer;
+          break;
+      }
+      roles.add(Role(id: role["id"], name: name));
+    }
+  }
 
   void handleLoginResponse(http.Response response) {
     var body = response.body;
@@ -110,5 +153,48 @@ class ApiClient {
     _authenticationController.add(null);
   }
 
-  // ApiWebSocket here
+  Future<void> getRoutes() async {
+    var response = await http.get(
+      Uri.http(url, "api/v1/routes"),
+      headers: {"Content-Type": "application/json"}
+    );
+
+    var body = response.body;
+    dynamic json;
+
+    try {
+      json = jsonDecode(response.body);
+    } catch (_) {
+      throw body;
+    }
+    List<LineRoute> routes = [];
+    for (var route in json['routes']) {
+      String id = route['id'];
+      String name = route['name'];
+
+      VehicleType type = VehicleType.Bus;
+      switch (route['type']) {
+        case "Bus":
+          type = VehicleType.Bus;
+          break;
+        case "SharedTaxi":
+          type = VehicleType.SharedTaxi;
+          break;
+      }
+
+      Lines lines = const Lines(name: "", points: []);
+      for (var line in route['lines']) {
+        lines.points.add(LatLng(
+            line['latitude'],
+            line['longitude']
+        ));
+      }
+
+      routes.add(LineRoute(
+          center: lines.points[0], lines: [lines], name: name,),
+      );
+    }
+
+
+  }
 }
