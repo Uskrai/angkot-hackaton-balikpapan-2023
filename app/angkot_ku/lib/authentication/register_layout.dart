6dfd58e7417676afd804f37e3c7ee087acb38c64
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 12:25:44 +0800

    ui : slicing register layout

--- app/angkot_ku/lib/authentication/register_layout.dart
+++ app/angkot_ku/lib/authentication/register_layout.dart
@@ -1,9 +1,190 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../constant.dart';
 
 class RegisterLayout extends StatelessWidget {
+  const RegisterLayout({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+                width: double.infinity,
+                height: size.height * 0.15,
+                decoration: const BoxDecoration(
+                    borderRadius : BorderRadius.only(
+                      topLeft: Radius.circular(0),
+                      topRight: Radius.circular(0),
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                    color : mPrimaryColor
+                )
+            ),
+            const SizedBox(height: 35),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 32),
+                    padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text('ANGKOTKU', textAlign: TextAlign.left, style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Poppins',
+                            fontSize: 29,
+                            fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height : 0),
+                        Text('Siap menjadi pemandumu', textAlign: TextAlign.left, style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Poppins',
+                            fontSize: 16,
+                            letterSpacing: 0,
+                            fontWeight: FontWeight.normal,
+                            height: 1.5),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(16),
+                    child: TextField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Kata Sandi',
+                        labelStyle: const TextStyle(
+                          color: Color.fromRGBO(26, 37, 42, 1),
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(0, 96, 194, 1),
+                            width: 1,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(0, 96, 194, 1),
+                            width: 1,
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 22, vertical: 15),
+                      ),
+                      controller: TextEditingController(),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(16),
+                    child: TextField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Kata Sandi',
+                        labelStyle: const TextStyle(
+                          color: Color.fromRGBO(26, 37, 42, 1),
+                          fontFamily: 'Poppins',
+                          fontSize: 14,
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(0, 96, 194, 1),
+                            width: 1,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(0, 96, 194, 1),
+                            width: 1,
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 22, vertical: 15),
+                      ),
+                      controller: TextEditingController(),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(16),
+                    child: DropdownButtonFormField<String>(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(0, 96, 194, 1),
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                      items: ['Option 1', 'Option 2', 'Option 3']
+                          .map((label) => DropdownMenuItem(
+                        value: label,
+                        child: Text(label),
+                      ))
+                          .toList(),
+                      onChanged: (value) {},
+                      hint: const Text('Select an option'),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 47,
+                    margin: const EdgeInsets.symmetric(horizontal: 16),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => RegisterLayout())
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        primary: const Color.fromRGBO(0, 96, 194, 1),
+                      ),
+                      child: const Text(
+                        'Daftar',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                          fontFamily: 'Poppins',
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
 
     );
   }
