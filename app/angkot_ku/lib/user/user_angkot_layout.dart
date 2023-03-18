6a750b4bba915a110ec805bfb5ea3dc49908e702
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 18:22:08 +0800

    feat(app) : shows menu from another layout

---
+++ app/angkot_ku/lib/user/user_angkot_layout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UserAngkotLayout extends StatefulWidget {
+  const UserAngkotLayout({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _UserAngkotLayoutState();
+}
+
+class _UserAngkotLayoutState extends State<UserAngkotLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("test"),
+      ),
+    );
+  }
+
+}
