9f379b24200d3792413c4fda335186c4802d7b71
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 14:49:01 +0800

    fix(app) : changes layout navigate

--- app/angkot_ku/lib/user/user_layout.dart
+++ app/angkot_ku/lib/user/user_layout.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 Future<void> checkPermission() async {
@@ -22,86 +21,13 @@ Future<void> checkPermission() async {
   }
 }
 
-class HomeUserLayout extends StatefulWidget {
+class HomeUserLayout extends StatelessWidget{
   const HomeUserLayout({super.key});
 
   @override
-  State<StatefulWidget> createState() => _HomeUserLayoutState();
-
-}
-
-class _HomeUserLayoutState extends State<HomeUserLayout>{
-  int _selectedIndex = 0;
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Menu Angkot',
-    ),
-    Text(
-      'Menu Bis',
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  PersistentBottomSheetController? _bottomSheetController;
-
-  void _showBottomSheet() {
-    _bottomSheetController = Scaffold.of(context).showBottomSheet(
-          (context) => Container(
-            height: 200,
-            color: Colors.red,
-            child: const Center(
-              child: Text('Ini adalah BottomSheet'),
-            ),
-          ),
-      elevation: 8.0,
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-            _showBottomSheet();
-        },
-        backgroundColor: Colors.blue,
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 8.0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.directions_bus),
-                onPressed: () {
-                  _onItemTapped(0);
-                },
-              ),
-            ),
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.directions_car),
-                onPressed: () {
-                  _onItemTapped(1);
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
+    return Container(
+
     );
   }
 
