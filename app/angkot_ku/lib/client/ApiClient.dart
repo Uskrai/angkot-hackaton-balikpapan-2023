6bab5fe59908404c7afb88d56ff76f97c1da63cc
NawfalHadi <nawfalhdi@gmail.com>
2023-03-18 13:38:53 +0800

    feat(app) : handling login response in api client

--- app/angkot_ku/lib/client/ApiClient.dart
+++ app/angkot_ku/lib/client/ApiClient.dart
@@ -1,13 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:angkot_ku/client/User.dart';
-import 'package:angkot_ku/client/VehicleType.dart';
-import 'package:angkot_ku/temp/route.dart';
-import 'package:latlong2/latlong.dart';
-
 import 'Authenticated.dart';
 import 'package:http/http.dart' as http;
+import 'package:latlong2/latlong.dart';
 
 import 'Role.dart';
 
@@ -16,64 +12,16 @@ enum AuthenticationStatus {
   unkown
 }
 
-enum RouteStatus {
-  loaded,
-  unkown
-}
 class ApiClient {
   ApiClient({required this.url});
   final String url;
   final _authenticationController = StreamController();
-  final _routesController = StreamController();
 
   Authenticated? auth;
-  RoleRoute? route;
 
   Stream get authenticationStream => _authenticationController.stream;
-  Stream get routeStream => _routesController.stream;
 
   var authenticationStatus = AuthenticationStatus.unkown;
-  var routeType = DriverType.bus;
-  var routeStatus = RouteStatus.unkown;
-
-  void handleRoutesResponse(http.Response response) {
-    var body = response.body;
-    dynamic json;
-
-    try {
-      json = jsonDecode(response.body);
-    } catch (_) {
-      throw body;
-    }
-
-    var error = json["message"];
-    if (error != null) {
-      throw error;
-    }
-
-    var data = json["data"];
-    var session = data["session"];
-
-    List<Role> roles = [];
-    for (var role in data["roles"]) {
-      RoleType name;
-      switch (role["name"]) {
-        case "Customer":
-          name = RoleType.customer;
-          break;
-        case "Shared Taxi":
-          name = RoleType.sharedTaxi;
-          break;
-        case "Bus":
-          name = RoleType.bus;
-          break;
-        default:
-          name = RoleType.customer;
-          break;
-      }
-      roles.add(Role(id: role["id"], name: name));
-    }
-  }
 
   void handleLoginResponse(http.Response response) {
     var body = response.body;
@@ -102,13 +50,9 @@ class ApiClient {
           break;
         case "Shared Taxi":
           name = RoleType.sharedTaxi;
-          _routesController.add(null);
-          routeType = DriverType.sharedTaxi;
           break;
         case "Bus":
           name = RoleType.bus;
-          _routesController.add(null);
-          routeType = DriverType.bus;
           break;
         default:
           name = RoleType.customer;
@@ -120,108 +64,8 @@ class ApiClient {
     auth = Authenticated(session: session, roles: roles);
     _authenticationController.add(null);
     authenticationStatus = AuthenticationStatus.authenticated;
-
-    getRoutes();
   }
 
-  Future<void> signIn(String email, String password) async {
-    var response = await http.post(
-      Uri.http(url, "api/v1/auth/login"),
-      body: jsonEncode(
-        {"email": email, "password": password},
-      ),
-      headers: {"Content-Type": "application/json"},
-    );
 
-    handleLoginResponse(response);
-  }
-
-  Future<void> signUp(
-      String email,
-      String password,
-      List<RoleType> role,
-      ) async {
-    var response = await http.post(Uri.http(url, "api/v1/auth/register"),
-        body: jsonEncode(
-          {
-            "email": email,
-            "password": password,
-            "roles": role.map((e) {
-              switch (e) {
-                case RoleType.customer:
-                  return {"id": 1};
-                case RoleType.sharedTaxi:
-                  return {"id": 2};
-                case RoleType.bus:
-                  return {"id": 3};
-              }
-            }).toList()
-          },
-        ),
-        headers: {"Content-Type": "application/json"});
-
-    handleLoginResponse(response);
-  }
-
-  Future<void> logOut() async {
-    authenticationStatus = AuthenticationStatus.unkown;
-    auth = null;
-    _authenticationController.add(null);
-  }
-
-  Future<void> getRoutes() async {
-    var response = await http.get(
-      Uri.http(url, "api/v1/route"),
-      headers: {"Content-Type": "application/json"}
-    );
-
-    var body = response.body;
-    dynamic json;
-
-    try {
-      json = jsonDecode(response.body);
-    } catch (_) {
-      print(body);
-      throw body;
-    }
-    var routes = RoleRoute(bus: [], sharedTaxi: []);
-    for (var route in json['routes']) {
-      String id = route['id'];
-      String name = route['name'];
-
-      VehicleType type = VehicleType.Bus;
-      switch (route['type']) {
-        case "Bus":
-          type = VehicleType.Bus;
-          break;
-        case "SharedTaxi":
-          type = VehicleType.SharedTaxi;
-          break;
-      }
-
-      Lines lines = const Lines(name: "", points: []);
-      for (var line in route['lines']) {
-        lines.points.add(LatLng(
-            line['latitude'],
-            line['longitude']
-        ));
-      }
-
-      switch (type) {
-
-        case VehicleType.Bus:
-          routes.bus.add(LineRoute(type: type, lines: [lines], name: name));
-          break;
-        case VehicleType.SharedTaxi:
-          routes.sharedTaxi.add(LineRoute(type: type, lines: [lines], name: name));
-
-          break;
-      }
-    }
-
-    print(routes);
-    route = routes;
-    _routesController.add(null);
-    routeStatus = RouteStatus.loaded;
- }
+  // ApiWebSocket here
 }
